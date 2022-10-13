78b3a4a322381a53e89370261709d592713b9b7b
yaddysantos <katysantos424@gmail.com>
2022-10-13 11:26:17 -0500

    creacion proyecto

---
+++ front-iris/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SelectorComponent } from './selector/selector.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SelectorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
